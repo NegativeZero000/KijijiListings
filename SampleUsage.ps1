5ef6f8d484c751925efb0ca10b9671bd60314e89
Matt Cameron <negativezero000@gmail.com>
2018-01-07 11:53:00 -0500

    Updated sample usage scripts
    Added more examples and comments.

--- SampleUsage.ps1
+++ SampleUsage.ps1
@@ -1,22 +1,42 @@
-﻿
+﻿# Import the module. 
+# Load this into a common module location as defined by $env:PSModulePath
+# or use an absolute path
+Import-Module M:\Code\KijijiListings\KijijiListings\Kijiji-Listings.psm1
 
+# Note the region comments will appear to be doubled. This is so I can see it easier on my darktheme in ISE
+# Bug: https://windowsserver.uservoice.com/forums/301869-powershell/suggestions/18838177-collapsed-region-in-dark-mode-is-hardly-visible
 
-# Format string 
-# 0 - The actual search string in kijiji
-# 1 - Optional page. as page-#/
+# region Simple search
+#region Simple search
+# A manual search of kijiji is required to get the correct location and category.
+$kijijiSearchURL = "https://www.kijiji.ca/b-toys-games/ottawa/monopoly/k0c108l1700185r60.0"
+$searchListing = Get-KijijiURLListings -BaseUrl $kijijiSearchURL 
 
-$kijijiSearchURL = "https://www.kijiji.ca/b-toys-games/ottawa/{0}/k0c108l1700185r60.0?address=Arnprior&ll=45.434745,-76.351847"
-$allListingResults = New-Object "System.Collections.ArrayList"
+# Display information of the results.
+Write-Host "Total listings: $($listing.TotalNumberOfSearchResults)" -ForegroundColor Green
+$listing.Listings
+#endregion
 
-$listing= Get-KijijiURLListings -PlaceholderUrl $kijijiSearchURL -SearchString yahtzee
-"Total listings: $($listing.TotalNumberOfSearchResults)"
-$allListingResults.AddRange(@($listing.Listings))
+# region Keyword seach
+#region Keyword seach
+# A manual search of kijiji is required to get the correct location and category.
+# You can use a placeholder URL which allows the insertion of search terms.
+$kijijiSearchURL = "https://www.kijiji.ca/b-toys-games/ottawa/{0}/k0c108l1700185r60.0"
+$searchListing = Get-KijijiURLListings -PlaceholderUrl $kijijiSearchURL -SearchString yahtzee
 
-#while ($listing.hasMorePages()) {
-#   $listing =  Get-KijijiURLListings $listing.nextPageUrl
-#   $allListingResults.AddRange($listing.Listings)
-#}
+# Display information of the results.
+Write-Host "Total listings: $($listing.TotalNumberOfSearchResults)" -ForegroundColor Green
+$listing.Listings
+#endregion
 
-# $allListingResults | select title,distance,price,image
-
+# region Simple search image gallery
+#region Simple search image gallery
+# A manual search of kijiji is required to get the correct location and category.
+# The output of this searches images will be displayed as an image gallery with clickable 
+# links to the actual posts. 
+$kijijiSearchURL = "https://www.kijiji.ca/b-toys-games/ottawa/monopoly/k0c108l1700185r60.0"
+$searchListing = Get-KijijiURLListings -BaseUrl $kijijiSearchURL 
+# Display information of the results.
+Write-Host "Total listings: $($listing.TotalNumberOfSearchResults)" -ForegroundColor Green
 $listing | Out-KijijiGridView
+#endregion
