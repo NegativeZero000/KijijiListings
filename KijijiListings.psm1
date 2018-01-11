875a5bd1fcd0a66ea4180cb4e2b43aedf807de32
Matt Cameron <negativezero000@gmail.com>
2018-01-11 16:15:24 -0500

    Added verbose help for cmdlets in main module.
    also discovered small bug which needs to be mitigated. https://github.com/NegativeZero000/KijijiListings/issues/2

--- KijijiListings.psm1
+++ KijijiListings.psm1
@@ -1,10 +1,27 @@
 ﻿Add-Type -AssemblyName System.Web
 Add-Type -AssemblyName System.Windows.Forms
 
+<#
+.SYNOPSIS
+    Parses webcontent for meta data to describe a search 
+.DESCRIPTION
+    Gather information via webscraping from search results for statistics on the search itself. 
+    These are useful for determining how many items were found and if there are more on other pages.
+.PARAMETER HTMLString
+    String containing the raw HTML from a web page.
+.EXAMPLE
+    $listingMetaData = $scrapedText | Get-SearchListingMetaData
+.EXAMPLE
+    Get-SearchListingMetaData -HTML $scrapedText
+.INPUTS
+   System.String. You can pipe HTML strings to Get-SearchListingMetaData
+.OUTPUTS
+   System.Collections.Hashtable. Get-SearchListingMetaData returns a hashtable of navigation based findings.
+#>
 function Get-SearchListingMetaData{
     param(
         [parameter(ValueFromPipeLine=$true)]
-        $ListingString
+        [string]$HTMLString
     )
     $searchMetaData = @{}
     $numberofListingsRegex = '(?sm)<div class="showing">.*?Showing (?<FirstListingResultIndex>[\d,]+) - (?<LastListingResultIndex>[\d,]+) of (?<TotalNumberOfSearchResults>[\d,]+) Ads</div>'
@@ -15,7 +32,7 @@ function Get-SearchListingMetaData{
     $searchMetaData.TotalNumberOfSearchResults = 0
 
 
-    if($ListingString -match $numberofListingsRegex ){
+    if($HTMLString -match $numberofListingsRegex ){
         $searchMetaData.FirstListingResultIndex    = $Matches["FirstListingResultIndex"] -as [int]
         $searchMetaData.LastListingResultIndex     = $Matches["LastListingResultIndex"]  -as [int]
         $searchMetaData.TotalNumberOfSearchResults = $Matches["TotalNumberOfSearchResults"]  -as [int]
@@ -23,11 +40,25 @@ function Get-SearchListingMetaData{
     return $searchMetaData
 }
 
+<#
+.SYNOPSIS
+    Get the current Kijiji page in a given search.
+.DESCRIPTION
+    Parses a URI to get the page number in a Kijiji search. If no page number is present then 1 is assumed.
+.PARAMETER URL
+    A Kijiji search address
+.EXAMPLE
+    Get-KijijiURLPageNumber $Kijijiwebaddress
+.INPUTS
+   None. You cannot pipe objects to Get-KijijiURLPageNumber
+.OUTPUTS
+   System.Int32. The page number found in a URI segement or 1
+#>
 function Get-KijijiURLPageNumber{
     param(
-        [validatePattern("^https://www\.kijiji\.ca")]
-        [validatescript({$_ -as [uri]})]
-        [string]$URL
+        [validateScript({$_.Host -eq "www.kijiji.ca"})]
+        [Alias("URI")]
+        [uri]$URL
     )
 
     # Check to see if this url has a page number component. 
@@ -37,47 +68,83 @@ function Get-KijijiURLPageNumber{
 
 
     # Pull the page number out of the segment "page-##/"
-    $URI = $URL -as [uri]
-    # [int]$pageNumber = $URI.segments | Where-Object{$_ -match "page\-(\d+)"} | Select-Object -Last 1 | ForEach-Object{$Matches[1]}
-    [int]$pageNumber = if($URI.segments[4] -match "page\-(\d+)"){$Matches[1]}else{1}
+    [int]$pageNumber = if($URL.segments[4] -match "page\-(\d+)"){$Matches[1]}else{1}
 
     return $pageNumber
 }
 
+<#
+.SYNOPSIS
+    Set a given page number into a given Kijiji search.
+.DESCRIPTION
+    Parses a URI to get the page number in a Kijiji search. If no page number is present then 1 is assumed.
+    This will not validate if the search will return anything of value. 
+.PARAMETER URL
+    A Kijiji search address
+.PARAMETER PageNumber
+    A new non-zero integer that will be used as the new page number.
+.EXAMPLE
+    Set-KijijiURLPageNumber $Kijijiwebaddress -PageNumber 2
+.INPUTS
+   None. You cannot pipe objects to Set-KijijiURLPageNumber
+.OUTPUTS
+   System.String. A rebuilt search URL is returned
+#>
 function Set-KijijiURLPageNumber{
+    [cmdletbinding()]
     param(
-        [validatePattern("^https://www\.kijiji\.ca")]
-        [validatescript({$_ -as [uri]})]
-        [string]$URL,
+        [validateScript({$_.Host -eq "www.kijiji.ca"})]
+        [Alias("URI")]
+        [uri]$URL,
 
         [ValidateScript({$_ -gt 0})]
+        [Alias("Number")]
         [int]$PageNumber
     )
 
-    $URI = $URL -as [uri]
-
     # Rebuild the url based on the URI parts. 
+    Write-Verbose "Adjusting page of: '$URL'"
+    Write-Verbose "Segments: $($URL.Segments.Count) - '$($URL.Segments -join '')'"
     # Edit the segments to either add a page designation or replace the existing one.
-    $updatedSegments = 0..($URI.Segments.Count - 1) | ForEach-Object{
+    $updatedSegments = 0..($URL.Segments.Count - 1) | ForEach-Object{
         switch($_){
             # It seems that the page number is always the 5th segement in the URI. 
             4{
+                Write-Verbose "URI Segment 4: $($URL.Segments[$_])"
                 # Substitute this segment with a new page number
                 "page-$PageNumber/"
 
                 # If this segment is not already a page number then add this back to the segment chain.
-                if($URI.Segments[$_] -notmatch "page\-\d+"){
-                    $URI.Segments[$_]
+                if($URL.Segments[$_] -notmatch "page\-\d+"){
+                    Write-Verbose "Added non-page segment back to chain"
+                    $URL.Segments[$_]
                 }
             }
-            default{$URI.Segments[$_]}
+            default{$URL.Segments[$_]}
         }
     }
 
     # Rebuild the URI and return
-    return ([System.UriBuilder]::new($URI.Scheme,$URI.Host,$URI.port, -join $updatedSegments,$URI.Query)).Uri.AbsoluteUri
+    return ([System.UriBuilder]::new($URL.Scheme,$URL.Host,$URL.port, -join $updatedSegments,$URL.Query)).Uri.AbsoluteUri
 }
 
+<#
+.SYNOPSIS
+    Parses webcontent for meta data to describe a search 
+.DESCRIPTION
+    Gather information via webscraping from search results for statistics on the search itself. 
+    These are useful for determining how many items were found and if there are more on other pages.
+.PARAMETER HTMLString
+    String containing the raw HTML from a web page.
+.EXAMPLE
+    $listingMetaData = $scrapedText | Get-SearchListingMetaData
+.EXAMPLE
+    Get-SearchListingMetaData -HTML $scrapedText
+.INPUTS
+   System.String. You can pipe HTML strings to Get-SearchListingMetaData
+.OUTPUTS
+   System.Collections.Hashtable. Get-SearchListingMetaData returns a hashtable of navigation based findings.
+#>
 function Convert-KijijiListingToObject{
     param(
         [parameter(
@@ -138,6 +205,29 @@ function Convert-KijijiListingToObject{
     }
 }
 
+<#
+.SYNOPSIS
+    Creates a search object containing meta data and listings from a Kijiji web search
+.DESCRIPTION
+    Collects information from other module cmdlets to build one final search object that has information
+    about the search itself as well as the individual listings found.
+    Slightly more complex searches can be done with preconfigured strings that allow for mulitple kijiji seaches 
+    within the same location and category.
+.PARAMETER BaseUrl
+    String containing the Kjiji search URL.
+.PARAMETER PlaceholderUrl
+    String containing the Kjiji search URL with a format placeholder to allow for search strings to be used.
+.PARAMETER SearchString
+    String used to limit search much the same as the interactive site.
+.EXAMPLE
+    $searchListing = Get-KijijiURLListings -BaseUrl $kijijiSearchURL 
+.EXAMPLE
+    Get-KijijiURLListings -PlaceholderUrl "https://www.kijiji.ca/b-toys-games/ottawa/{0}/k0c108l1700185r60.0" -SearchString yahtzee
+.INPUTS
+   System.String. You can pipe HTML strings to Get-SearchListingMetaData
+.OUTPUTS
+   System.Management.Automation.PSCustomObject. Get-KijijiURLListings returns a custom Kijiji.SearchResult object
+#>
 function Get-KijijiURLListings{
     [cmdletbinding(DefaultParameterSetName="Simple")]
     param(
@@ -159,10 +249,7 @@ function Get-KijijiURLListings{
             Mandatory,
             ParameterSetName="Formatted",
             Position=1)]
-        $SearchString,
-
-        [Parameter(Mandatory=$false)]
-        [switch]$All
+        $SearchString
     )
 
     # Get the initial text from the page
